8b1044c16659b3ad51429a027db79e779bb0bc78
Anurag Lohar <109411443+Githubak2002@users.noreply.github.com>
2023-11-19 03:10:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cors());
 
 
 // ==== static folder ====
-// app.use(express.static(path.join(__dirname,'/client/dist')));
+app.use(express.static(path.join(__dirname,'/client/dist')));
 // app.get('*',(req,res) => {
 //   res.sendFile(path.join(__dirname,"client", "dist", "index.html"));
 // })
